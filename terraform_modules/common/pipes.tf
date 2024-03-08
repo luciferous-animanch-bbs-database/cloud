3b3d4449cba3b5987513605cd0061ec5d4c1177c
sinofseven <em.s.00001@gmail.com>
2024-03-09 03:36:04 +0900

    fix

--- terraform_modules/common/pipes.tf
+++ terraform_modules/common/pipes.tf
@@ -23,9 +23,9 @@ resource "terraform_data" "insert_thread_event_names" {
 }
 
 resource "aws_pipes_pipe" "insert_thread" {
-  source   = aws_dynamodb_table.threads.stream_arn
-  target   = aws_sqs_queue.insert_thread.arn
-  role_arn = aws_iam_role.pipes_dynamodb_to_sqs.arn
+  source        = aws_dynamodb_table.threads.stream_arn
+  target        = aws_sqs_queue.insert_thread.arn
+  role_arn      = aws_iam_role.pipes_dynamodb_to_sqs.arn
   desired_state = true ? "RUNNING" : "STOPPED"
 
   source_parameters {
